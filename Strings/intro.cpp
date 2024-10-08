b15b63f09f30bb7d0ce9f72a7f6f715effd9c5e3
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-10-08 19:54:06 +0530

    commit

--- Strings/intro.cpp
+++ Strings/intro.cpp
@@ -12,8 +12,11 @@ int main()
 
     // declaring the string with a particular character of given length
     string str1(5, 'a'); // it gives the output 5 times a
-    cout << str1;
+    cout << str1 << endl;
 
-    int a = str.length();
-    cout << a;
+    // taking string input with spaces
+    string str2;
+    cout << "Enter The 2nd string: ";
+    getline(cin, str2);
+    cout << "Str2 is: " << str2 << endl;
 }
