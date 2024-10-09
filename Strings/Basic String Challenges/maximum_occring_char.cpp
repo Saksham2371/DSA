21abe5f3a03fff48bbf5c49811202a14309b48ea
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-10-09 20:23:05 +0530

    s

--- Strings/Basic String Challenges/maximum_occring_char.cpp
+++ Strings/Basic String Challenges/maximum_occring_char.cpp
@@ -10,13 +10,13 @@ int main()
     int freq[26];
 
     for (int i = 0; i < 26; i++)
-    {
         freq[i] = 0;
-        for (int i = 0; i < str.size(); i++)
-        {
-            freq[str[i] - 'a']++;
-        }
+
+    for (int i = 0; i < str.size(); i++)
+    {
+        freq[str[i] - 'a']++;
     }
+
     char ans = 'a';
     int maxF = 0;
 
