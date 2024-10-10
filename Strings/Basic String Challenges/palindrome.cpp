30b85095a907543fb8f2a58bbae9bbd729bb0aaf
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-10-10 20:05:26 +0530

    s

---
+++ Strings/Basic String Challenges/palindrome.cpp
@@ -0,0 +1,40 @@
+#include "iostream"
+#include "string"
+#include "algorithm"
+using namespace std;
+
+class palindrome
+{
+public:
+    bool pali(string str)
+    {
+        int start = str[0];
+        int end = str[str.size() - 1];
+
+        while (start < end)
+        {
+            if (str[start] != str[end])
+                return false;
+            else
+            {
+                return true;
+            }
+            start++;
+            end--;
+        }
+    }
+};
+
+int main()
+{
+
+    string str = "naman";
+
+    palindrome p;
+
+    if(p.pali(str)){
+        cout << "palindrome" << endl;
+    }
+
+    return 0;
+}
