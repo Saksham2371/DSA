8be20d5997844c58431a3ac390b1979414eca61c
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-10-20 19:33:02 +0530

    dddd

---
+++ Strings/Problems/shortestDistance.cpp
@@ -0,0 +1,38 @@
+#include "iostream"
+#include "vector"
+#include "string"
+#include "climits"
+using namespace std;
+
+int shortestdistance(vector<string> &s, string w1, string w2)
+{
+    int d1 = -1;
+    int d2 = -1;
+
+    int ans = INT_MAX;
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] == w1)
+        {
+            d1 = i;
+        }
+        if (s[i] == w2)
+        {
+            d2 = i;
+        }
+        if (d1 != -1 && d2 != -1)
+        {
+            ans = min(ans, abs(d1 - d2));
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<string> s = {"the", "animal", "fox", "quack", "duck"};
+    string word1 = "animal";
+    string word2 = "duck";
+    cout << shortestdistance(s, word1, word2) << endl;
+    return 0;
+}
