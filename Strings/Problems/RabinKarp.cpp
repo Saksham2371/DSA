e6a790ac539ba58603eabb08729ab7deec23840e
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-11-04 20:10:55 +0530

    ms

---
+++ Strings/Problems/RabinKarp.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int rabinKarp(string s1, string s2)
+{
+    return 0;
+}
+int main()
+{
+    string s1 = "na";
+    string s2 = "apna colledgena";
+    cout << "1st String occured" << rabinKarp(s1, s2) << " times" << endl;
+    return 0;
+}
