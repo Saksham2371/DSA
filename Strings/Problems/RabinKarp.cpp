fced7cc62c09d353387a85b89f0f52e4b52e0205
Saksham2371 <saksham0620.be20@chitkara.edu.in>
2024-11-05 20:06:39 +0530

    sfs

--- Strings/Problems/RabinKarp.cpp
+++ Strings/Problems/RabinKarp.cpp
@@ -8,6 +8,8 @@ int main()
 {
     string s1 = "na";
     string s2 = "apna colledgena";
+    int s11 = s1.size();
+    int s21 = s2.size();
     cout << "1st String occured" << rabinKarp(s1, s2) << " times" << endl;
     return 0;
 }
